f43a6151675f7c0716b9a62933014ae138494db5
KumarBibekAnand-2023MT93273 <2023mt93273@wilp.bits-pilani.ac.in>
2024-05-12 02:32:19 +0530

    Messaging with user functionality integrated

--- src/main/java/com/fsad/bookexchange/controllers/MessageController.java
+++ src/main/java/com/fsad/bookexchange/controllers/MessageController.java
@@ -3,6 +3,7 @@ package com.fsad.bookexchange.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fsad.bookexchange.entity.Message;
 import com.fsad.bookexchange.service.MessageService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class MessageController {
@@ -25,7 +27,7 @@ public class MessageController {
 		return msgService.addMessage(msg);
 	}
 	
-	@GetMapping("/message")
+	@GetMapping("/message/{recepientId}")
 	public List<Message> getMessage(@PathVariable String recepientId) {
 		return msgService.getMessage(recepientId);
 	}
